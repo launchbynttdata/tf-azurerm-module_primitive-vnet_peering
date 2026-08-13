db0378d4796e535d4e392ea4c1ec580644ca84a4
rakesh gorige <rakesh.gorige@resideo.com>
2026-08-13 16:42:47 -0500

    fix: resolve CI failures after copier conversion

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -12,7 +12,7 @@
 
 module "resource_names" {
   source  = "terraform.registry.launch.nttdata.com/module_library/resource_name/launch"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   for_each = var.resource_names_map
 
